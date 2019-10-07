6f3fc60742a6eee685d81c8d4af1c793d881ae76
atmoroz <atmoroz93@gmail.com>
2019-10-07 22:40:57 +0300

    state

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,11 +4,57 @@ import Movie from '../Movie/Movie';
 import './Main.css';
 
 class Main extends React.Component {
+
+    async componentDidMount() {
+        await Promise.all([this.gettingMovie(), this.gettingGenres()]);
+    }
+
+    state = {
+        films: [],
+        genres: {}
+    }
+    
+    gettingMovie = async() => {
+        const apiUrlFilm = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=3ba8aa36d9cc706beaa7cd4d393225a3&language=en-US&page=1');
+        const dataFilm = await apiUrlFilm.json();
+        this.setState( { films: dataFilm.results } );
+    }
+
+    gettingGenres = async() => {
+        const apiGenres = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3ba8aa36d9cc706beaa7cd4d393225a3&language=en-US');
+        const dataGenres = await apiGenres.json();
+        this.setState({
+            genres: dataGenres.genres.reduce((start, item) => ({
+                ...start,
+                [item.id]: item.name
+            }), {})
+        })
+    }
+
+    
+
+    renderMovies = (item => {
+        return(
+            <Movie
+                key={item.id} 
+                title={item.title}
+                posters={item.poster_path}
+                year={item.release_date}
+                rating={ Number.isInteger(item.vote_average) ? item.vote_average+'.0' : item.vote_average }
+                genres={
+                    item.genre_ids.map((item, i, arr) => {
+                        return (i < arr.length - 1) ?  this.state.genres[item] + ', ' : this.state.genres[item];
+                    })
+                }
+            />
+        )
+    })
+
     render() {
         return(
             <section className="main">
-                <div className="main_content layout">                        
-                    <Movie />
+                <div className="main_content layout">
+                    { this.state.films.map(this.renderMovies) }                  
                 </div>
             </section>
         );
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,159 +3,26 @@ import React from 'react';
 import './Movie.css';
 
 class Movie extends React.Component {
+    
     render() {
+        const {title, posters, year, rating, genres} = this.props;
         return(
-            <div className='movieWrap'>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
+            <div className="movie_preview">
+                <div className="movie_img">
+                    <img className="movie_img__img" src={`https://image.tmdb.org/t/p/w200${posters}`} alt="logan" width="100%" />
+                    <span className="movie_year">{year.split('-')[0]}</span>
                 </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
+                <div className="movie_description">
+                    <div className="movie_description__item">
+                        <h2 className="movie_name">
+                            {title}
+                        </h2>
+                        <div className="movie_ganre">
+                            {genres}
                         </div>
                     </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
-                    </div>
-                </div>
-                <div className="movie_preview">
-                    <div className="movie_img">
-                        <img className="movie_img__img" src='./image/poster.jpg' alt="logan" width="100%" />
-                        <span className="movie_year">1992</span>
-                    </div>
-                    <div className="movie_description">
-                        <div className="movie_description__item">
-                            <h2 className="movie_name">
-                                Forsage
-                            </h2>
-                            <div className="movie_ganre">
-                                active
-                            </div>
-                        </div>
-                        <div className="movie_rating">
-                            4.5
-                        </div>
+                    <div className="movie_rating">
+                        {rating}
                     </div>
                 </div>
             </div>
